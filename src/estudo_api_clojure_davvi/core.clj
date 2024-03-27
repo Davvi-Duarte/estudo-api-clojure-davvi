8baf816254d991df7d505c1367aaf19e982080a7
Davvi Duarte Rodrigues <davvi.duarte@gmail.com>
2024-03-27 00:53:31 -0300

    criando hello world básico utilizando o servidor http

--- src/estudo_api_clojure_davvi/core.clj
+++ src/estudo_api_clojure_davvi/core.clj
@@ -1,6 +1,16 @@
-(ns estudo-api-clojure-davvi.core)
+(ns estudo-api-clojure-davvi.core
+  (:require [io.pedestal.http.route :as route]
+            [io.pedestal.http :as http]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(defn funcao-hello [request]
+    {:status 200 :body (str "Hello World " (get-in request [:query-params :name] "Everybody"))})
+(def routes (route/expand-routes
+              #{["/hello" :get funcao-hello :route-name :hello-world ]}))
+(def service-map {::http/routes routes
+                  ::http/port 9999
+                  ::http/type :jetty
+                  ::http/join? false});permiteo não travamento da thread do clojure. útil para desenvolvimento (testes)
+
+(http/start (http/create-server service-map))
+(println "Started server http")
